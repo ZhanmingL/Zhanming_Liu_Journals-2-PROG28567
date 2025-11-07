2e90f2d81ac0bc223cba9e178ee1e240817b7dd1
ZhanmingL <liuzhanm@sheridancollege.ca>
2025-11-07 17:20:09 -0500

    Done last research and finish comments

--- Assets/Scripts/Week9JournalScripts/AngularRotating.cs
+++ Assets/Scripts/Week9JournalScripts/AngularRotating.cs
@@ -16,5 +16,12 @@ public class AngularRotating : MonoBehaviour
         {
             rigidbody2D.angularDamping += 0.1f; //increase damping, angularVelocity slow down.
         }
+
+        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition); //Get mouse position
+        Vector2 closest = rigidbody2D.ClosestPoint(mousePos); //Smallest distance value between sqaure rigidbody and mouse position
+        Debug.DrawLine(mousePos, closest, Color.black); //draw a line between them to see
+
+        float distance = Vector2.Distance(mousePos, closest);
+        Debug.Log("smallest distance to sqaure is" + distance); //print out distance value
     }
 }
