2d5b6f4113b1bc84f41eb5e7c5a1a5f5a6ade783
ZhanmingL <liuzhanm@sheridancollege.ca>
2025-11-06 20:58:20 -0500

    Done research 2&3
    Did workable codes of angular velocity and angular damping.

---
+++ Assets/Scripts/Week9JournalScripts/AngularRotating.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class AngularRotating : MonoBehaviour
+{
+    Rigidbody2D rigidbody2D;
+
+    void Start()
+    {
+        rigidbody2D = transform.GetComponent<Rigidbody2D>();
+        rigidbody2D.angularVelocity = 120;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            rigidbody2D.angularDamping += 0.1f;
+        }
+    }
+}
