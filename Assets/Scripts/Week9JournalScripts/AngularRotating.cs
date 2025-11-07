99481a149d67349be78cd97d7588188d613b307f
ZhanmingL <liuzhanm@sheridancollege.ca>
2025-11-07 16:41:07 -0500

    Comments

--- Assets/Scripts/Week9JournalScripts/AngularRotating.cs
+++ Assets/Scripts/Week9JournalScripts/AngularRotating.cs
@@ -2,19 +2,19 @@ using UnityEngine;
 
 public class AngularRotating : MonoBehaviour
 {
-    Rigidbody2D rigidbody2D;
+    Rigidbody2D rigidbody2D; //reference
 
     void Start()
     {
-        rigidbody2D = transform.GetComponent<Rigidbody2D>();
-        rigidbody2D.angularVelocity = 120f;
+        rigidbody2D = transform.GetComponent<Rigidbody2D>(); //reference is object that has this script (two sqaures)
+        rigidbody2D.angularVelocity = 120f; //rotating speed is 120.
     }
 
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0)) //if press mouse's left button
         {
-            rigidbody2D.angularDamping += 0.1f;
+            rigidbody2D.angularDamping += 0.1f; //increase damping, angularVelocity slow down.
         }
     }
 }
--- Assets/Scripts/Week9JournalScripts/MouseBoxCheck.cs
+++ Assets/Scripts/Week9JournalScripts/MouseBoxCheck.cs
@@ -2,13 +2,13 @@ using UnityEngine;
 
 public class MouseBoxCheck : MonoBehaviour
 {
-    public Vector2 boxSize = new Vector2(3, 3);
-    public LayerMask targetLayer;
-    bool check = false;
+    public Vector2 boxSize = new Vector2(3, 3); //The boxCast covers a 3x3 box range
+    public LayerMask targetLayer; //Detect rigidbody in target layer
+    bool check = false; //false when not found any rigidbodies
 
     void Update()
     {
-        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition); //mousePos as the middle point of boxCast
         transform.position = mousePos;
 
         check = Physics2D.BoxCast(transform.position, boxSize, 0, Vector2.zero, 0, targetLayer);
@@ -17,6 +17,7 @@ public class MouseBoxCheck : MonoBehaviour
         {
             drawColor = Color.green;
         }
+        //Draw a square where boxCast is
         Debug.DrawLine(new Vector2(mousePos.x - boxSize.x / 2, mousePos.y + boxSize.y / 2), mousePos + boxSize / 2, drawColor);
         Debug.DrawLine(mousePos + boxSize / 2, new Vector2(mousePos.x + boxSize.x / 2, mousePos.y - boxSize.y / 2), drawColor);
         Debug.DrawLine(new Vector2(mousePos.x + boxSize.x / 2, mousePos.y - boxSize.y / 2), mousePos - boxSize / 2, drawColor);
